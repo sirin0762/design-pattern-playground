e35f2cd95d6580db72b7d81144d06da7ba8949db
sirin0762 <tlsdydwls123@gmail.com>
2022-01-12 20:12:00 +0900

    FEAT : chapter 8 추가

---
+++ chapter8/src/CaffeineBeverage.java
@@ -0,0 +1,24 @@
+package chapter8;
+
+public abstract class CaffeineBeverage {
+
+    final void prepareRecipe() {
+        boilWater();
+        brew();
+        pourInCup();
+        addCondiments();
+    }
+
+    public void boilWater() {
+        System.out.println("물 끓이는 중");
+    }
+
+    public void pourInCup() {
+        System.out.println("컵에 따르는 중");
+    }
+
+    abstract void brew();
+
+    abstract void addCondiments();
+
+}
