08043d32b7d9a734688731ff2f89297c10693460
sirin0762 <tlsdydwls123@gmail.com>
2022-02-10 16:38:02 +0900

    FEAT : chapter 10 지저분한 상태의 상태 패턴

---
+++ chapter10/src/GumballMachineTestDrive.java
@@ -0,0 +1,10 @@
+public class GumballMachineTestDrive {
+
+    public static void main(String[] args) {
+        GumballMachine gumballMachine = new GumballMachine(5);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+    }
+
+}
